4cad3112c08688ec1ba06006f852b586e9da70e9
nkazakova <natalia.kazakova@innoway.ru>
2019-06-28 13:34:09 +0300

    Добавлены две конфигурации для сборки платной и бесплатной версий приложения

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -13,8 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        createNewNote()
+        saveNote()
+    }
+    
+    // MARK: - Stub for creating notes function
+    func createNewNote() {
+        if getConfigValue("Notes limit") == "0" {
+            // Pay version
+            print("Pay version createNewNote")
+        } else {
+            // Free version
+            print("Free version createNewNote")
+        }
+    }
+    
+    // MARK: - Stub for saving notes function
+    func saveNote() {
+        if getConfigValue("Use remote data storage") == "YES" {
+            // Pay version
+            print("Pay version saveNote")
+        } else {
+            // Free version
+            print("Free version saveNote")
+        }
     }
 
+    func getConfigValue(_ key: String) -> String? {
+        return Bundle.main.infoDictionary?[key] as? String
+    }
 
 }
 
