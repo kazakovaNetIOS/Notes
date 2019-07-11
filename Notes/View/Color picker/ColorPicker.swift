2b82a48bc3cce3290a0ce9678b3c2c29b174212d
nkazakova <natalia.kazakova@innoway.ru>
2019-07-11 21:18:09 +0300

    Палитра вынесена в отдельный класс

--- Notes/View/Color picker/ColorPicker.swift
+++ Notes/View/Color picker/ColorPicker.swift
@@ -16,16 +16,17 @@ class ColorPicker: UIView {
     @IBOutlet weak var colorPickerFrame: UIView!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var hexValueColorLabel: UILabel!
+    @IBOutlet weak var paletteView: PaletteView!
     
     private var targetImageView: UIImageView!
-    private var gradientLayer: CAGradientLayer!
     internal var delegate: ColorPickerDelegate?
     
     private var location: CGPoint = CGPoint(x: 0, y: 0) {
         willSet {
-            if gradientLayer.frame.minX...gradientLayer.frame.maxX ~= newValue.x,
-                gradientLayer.frame.minY...gradientLayer.frame.maxY ~= newValue.y {
-                selectedColor = getColor(at: newValue)
+            if colorPickerFrame.bounds.minX...colorPickerFrame.bounds.maxX ~= newValue.x,
+                colorPickerFrame.bounds.minY...colorPickerFrame.bounds.maxY ~= newValue.y {
+                selectedColor = paletteView.getColor(at: newValue)
+                
                 moveCoursor(at: newValue)
             }
         }
@@ -35,6 +36,7 @@ class ColorPicker: UIView {
         willSet {
             selectedColorView.backgroundColor = newValue
             brightnessSlider.value = Float(newValue.rgba.alpha)
+            paletteView.brightness = CGFloat(newValue.rgba.alpha)
             
             hexValueColorLabel.text = newValue.toHexString()
         }
@@ -53,8 +55,6 @@ class ColorPicker: UIView {
     }
     
     override func draw(_ rect: CGRect) {
-        createGradientLayer()
-        
         createTargetImage()
     }
     
@@ -93,27 +93,6 @@ class ColorPicker: UIView {
         return nib.instantiate(withOwner: self, options: nil).first! as! UIView
     }
     
-    private func createGradientLayer() {
-        var colorSets = [CGColor]()
-        colorSets.append(UIColor.red.cgColor)
-        colorSets.append(UIColor.orange.cgColor)
-        colorSets.append(UIColor.yellow.cgColor)
-        colorSets.append(UIColor.green.cgColor)
-        colorSets.append(UIColor.blue.cgColor)
-        colorSets.append(UIColor(red: 66.0/255, green: 170.0/255, blue: 255/255, alpha: 1).cgColor)
-        colorSets.append(UIColor(red: 139.0/255, green: 0/255, blue: 255/255, alpha: 1).cgColor)
-        colorSets.append(UIColor.white.cgColor)
-        colorSets.append(UIColor.black.cgColor)
-        
-        gradientLayer = CAGradientLayer()
-        gradientLayer.frame = colorPickerFrame.bounds
-        gradientLayer.colors = colorSets
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
-        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
-        
-        colorPickerFrame.layer.addSublayer(gradientLayer)
-    }
-    
     private func createTargetImage() {
         targetImageView = UIImageView(image: UIImage(named: "target"))
         
@@ -125,42 +104,12 @@ class ColorPicker: UIView {
     
     internal func rotateGradient() {
         colorPickerFrame.alpha = 0
-        gradientLayer.frame = colorPickerFrame.bounds
+        paletteView.rotate()
         targetImageView.isHidden = true
         
         UIView.animate(withDuration: 0.7) {
             self.colorPickerFrame.alpha = 1
         }
-    }
-    
-    private func getColor(at point: CGPoint) -> UIColor{
-        let pixel = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: 4)
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
-        
-        guard let context = CGContext(data: pixel, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace, bitmapInfo: bitmapInfo.rawValue) else {
-            return UIColor.white
-        }
-        
-        context.translateBy(x: -point.x, y: -point.y)
-        
-        gradientLayer.render(in: context)
-        
-        let color = UIColor(red:   CGFloat(pixel[0]) / 255.0,
-                            green: CGFloat(pixel[1]) / 255.0,
-                            blue:  CGFloat(pixel[2]) / 255.0,
-                            alpha: CGFloat(pixel[3]) / 255.0)
-        pixel.deallocate()
-        
-        return getColorWithAlpha(with: color)
-    }
-    
-    private func getColorWithAlpha(with color: UIColor) -> UIColor {
-        guard let slider = brightnessSlider else {
-            return color
-        }
-        
-        return color.withAlphaComponent(CGFloat(slider.value))
     }
     
     @IBAction func doneButtonTapped(_ sender: UIButton) {
